923220ab9aee69997b8a005d7c618f9515b31c7a
Sorachantha <xwsgbx@gmail.com>
2018-01-20 22:59:49 -0500

    Style: remove background colors

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import Message from './message';
 
-const MessageList = () => {
-  const tril = 'lightgrey';
-
-  return (
-    <View>
-      <Message bgcolor="white" name="David" message="Hey, how are you?" />
-      <Message bgcolor={tril} name="Tril" message="I'm fine, how are you man?" />
-      <Message bgcolor="white" name="David" message="I've been better" />
-      <Message bgcolor={tril} name="Tril" message="Tell me about it!" />
-      <Message bgcolor="white" name="David" message="I can't seem to catch a break!" />
-      <Message bgcolor={tril} name="Tril" message="Do you want to get a drink?" />
-      <Message bgcolor="white" name="David" message="Not tonight man..." />
-    </View>
-  );
-};
+const MessageList = () => (
+  <View>
+    <Message name="David" message="Hey, how are you?" />
+    <Message name="Tril" message="I'm fine, how are you man?" />
+    <Message name="David" message="I've been better" />
+    <Message name="Tril" message="Tell me about it!" />
+    <Message name="David" message="I can't seem to catch a break!" />
+    <Message name="Tril" message="Do you want to get a drink?" />
+    <Message name="David" message="Not tonight man..." />
+  </View>
+);
 
 export default MessageList;
