6e37fe5ac0bf885e1edd8bce6bc97980101aca48
Sorachantha <xwsgbx@gmail.com>
2018-01-20 22:19:18 -0500

    ft: List messages in main-chat

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react'; // the actual react code so the diff react codes and speak to each other
 import { Text, AppRegistry, View } from 'react-native'; // This renders it onto the mobile device
 import Newfile from './newfile';
+import Message from './src/components/message';
 
 const styles = {
   textStyle: {
@@ -15,11 +16,17 @@ const styles = {
 };
 
 const App = () => {
+  const tril = "lightgrey";
   const { textStyle, viewStyle } = styles;
   return (
-    <View style={viewStyle}>
-      <Text style={textStyle}>some text</Text>
-      <Text> Some more text </Text>
+    <View>
+      <Message bgcolor="white" name="David" message="Hey, how are you?" />
+      <Message bgcolor={tril} name="Tril" message="I'm fine, how are you man?" />
+      <Message bgcolor="white" name="David" message="I've been better" />
+      <Message bgcolor={tril} name="Tril" message="Tell me about it!" />
+      <Message bgcolor="white" name="David" message="I can't seem to catch a break!" />
+      <Message bgcolor={tril} name="Tril" message="Do you want to get a drink?" />
+      <Message bgcolor="white" name="David" message="Not tonight man..." />
     </View>
   );
 };
