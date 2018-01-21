b5fe056aabefba10af867e7c2297a002f97f6e8e
Sorachantha <xwsgbx@gmail.com>
2018-01-21 08:06:41 -0500

    Feat: add profile pics to messages

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { View } from 'react-native';
 import Message from './message';
+import davidsImage from '../images/david.jpeg';
+import trilsImage from '../images/tril.jpeg';
 
 const MessageList = () => (
   <View>
-    <Message name="David" message="Hey, how are you?" />
-    <Message name="Tril" message="I'm fine, how are you man?" />
-    <Message name="David" message="I've been better" />
-    <Message name="Tril" message="Tell me about it!" />
-    <Message name="David" message="I can't seem to catch a break!" />
-    <Message name="Tril" message="Do you want to get a drink?" />
-    <Message name="David" message="Not tonight man..." />
+    <Message
+      thumbnailImage={davidsImage}
+      name="David"
+      message="Hey, how are you?"
+    />
+    <Message
+      thumbnailImage={trilsImage}
+      name="Tril"
+      message="Im fine, how are you man?"
+    />
+    <Message
+      thumbnailImage={davidsImage}
+      name="David"
+      message="Ive been better"
+    />
+    <Message
+      thumbnailImage={trilsImage}
+      name="Tril"
+      message="Tell me about it!"
+    />
+    <Message
+      thumbnailImage={davidsImage}
+      name="David"
+      message="I can't seem to catch a break!"
+    />
+    <Message
+      thumbnailImage={trilsImage}
+      name="Tril"
+      message="Do you want to get a drink?"
+    />
+    <Message
+      thumbnailImage={davidsImage}
+      name="David"
+      message="Not tonight man..."
+    />
   </View>
 );
 
